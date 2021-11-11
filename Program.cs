71570d8d987ca84fd10d6f171901770e5e0d5012
Quinlan Walters <qwalters4@gmail.com>
2021-11-11 07:27:11 -0600

    unknownn drives will no longer have duplicates

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
+using System.Net;
 using System.Reflection;
 using System.Threading;
 
@@ -12,6 +13,7 @@ namespace PendingDriveProcessor
     {
         static void Main(string[] args)
         {
+            #region Check if Pending exists
             Console.Write("Current Directory: ");
             string curPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             Console.Write(curPath + "\n");
@@ -25,7 +27,9 @@ namespace PendingDriveProcessor
                 //Thread.Sleep(3000);
                 System.Environment.Exit(1);
             }
+            #endregion
 
+            #region Read every file from Pending and add to list
             List<PhysicalDisk> pending = new List<PhysicalDisk>();
 
             foreach (string file in Directory.EnumerateFiles(Path.Combine(@curPath, "Pending"), "*.json"))
@@ -33,60 +37,58 @@ namespace PendingDriveProcessor
                 PhysicalDisk temp = JsonConvert.DeserializeObject<PhysicalDisk>(File.ReadAllText(file));
                 pending.Add(temp);
             }
+            #endregion
 
+            #region Deserialize and build dictionary
             Dictionary<string, string> knownff = new Dictionary<string, string>();
+            List<string> unknown = new List<string>();
             string dic = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             knownff = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(Path.Combine(@dic, "KnownDriveDictionary.json")));
-            int i = 0;
+            #endregion
 
-            foreach(PhysicalDisk p in pending)
+            int i = 0;
+            bool found = false;
+
+            #region Check all disks and check if its in DB
+            foreach (PhysicalDisk p in pending)
             {
-                if(knownff.ContainsKey(p.ModelId))
+                foreach (KeyValuePair<string, string> kv in knownff)
                 {
-                    p.FormFactor = knownff[p.ModelId];
-                    i++;
-                    Console.WriteLine("ModelID: " + p.ModelId);
-                    Console.WriteLine("SerialNumber: " + p.SerialNumber + "\n");
+                    if (p.ModelId.Contains(kv.Key))
+                    {
+                        p.FormFactor = kv.Value;
+                        i++;
+                        found = true;
+                        break;
+                    }
                 }
+                if (!found)
+                {
+                    if(!unknown.Contains(p.ModelId))
+                    {
+                        Console.WriteLine("ModelID: " + p.ModelId);
+                        Console.WriteLine("SerialNumber: " + p.SerialNumber + "\n");
+                        unknown.Add(p.ModelId);
+                    }
+                    pending.Remove(p);
+                }
+                found = false;
             }
+            Console.WriteLine("Successfully parsed all pending drives.");
+            #endregion
+
+            File.WriteAllLines(Path.Combine(dic, "UnknownDrives.txt"), unknown);
 
             Console.WriteLine("Number of form factors changed: " + i);
 
-
-            DirectoryInfo di = new DirectoryInfo(Path.Combine(@curPath, "Pending"));
-            foreach (FileInfo file in di.EnumerateFiles())
-            {
-                file.Delete();
-            }
-
-            Console.WriteLine("Successfully parsed all pending drives.");
-            Console.WriteLine("Pushing drives to DB...");
-            //Thread.Sleep(2000);
-
-            //CALL WRITE TO DB
-            //string cstring;
-            //SqlConnection c;
-            //cstring = @"Data Source=WIN-50GP30FGO75;Initial Catalog=Demodb;User ID=sa;Password=demol23";
-            //c = new SqlConnection(cstring);
-            //c.Open();
-
-            //SqlCommand command;
-            //SqlDataAdapter adapter = new SqlDataAdapter();
-            //string sql = "";
-            //sql = "Insert into master (";
-            //command = new SqlCommand(sql, c);
-            //adapter.InsertCommand = new SqlCommand(sql, c);
-            //adapter.InsertCommand.ExecuteNonQuery();
-            //command.Dispose();
-            //c.Close();
-            //////////////////
-
-            Console.WriteLine("Push complete.");
+            #region Sort and write to Archive then delete from Pending
             Console.WriteLine("Archiving...");
-
-            foreach(PhysicalDisk p in pending)
+            string path;
+            foreach (PhysicalDisk p in pending)
             {
-                string path = Path.Combine(@curPath, "Archive", (p.SerialNumber + ".json"));
+                if (!Directory.Exists(Path.Combine(@curPath, "Archive", p.PONumber.ToString())))
+                    Directory.CreateDirectory(Path.Combine(@curPath, "Archive", p.PONumber.ToString()));
+                path = Path.Combine(@curPath, "Archive", p.PONumber.ToString(), (p.SerialNumber + ".json"));
                 JsonSerializer s = new JsonSerializer();
                 using (StreamWriter sw = new StreamWriter(path))
                 using (JsonWriter w = new JsonTextWriter(sw))
@@ -95,8 +97,15 @@ namespace PendingDriveProcessor
                 }
             }
 
+            DirectoryInfo di = new DirectoryInfo(Path.Combine(@curPath, "Pending"));
+            foreach (FileInfo file in di.EnumerateFiles())
+            {
+                    file.Delete();
+            }
+            #endregion
+
             Console.WriteLine("Complete");
-            Thread.Sleep(5000);
+            Thread.Sleep(20000);
         }
     }
 }
